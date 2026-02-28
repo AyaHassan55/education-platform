22f683d4ca8c10b52b56f4bc94b0ea7b5bfcb62b
AyaHassan55 <haya98166@gmail.com>
2026-03-01 01:01:18 +0200

    wrap context to all app

--- app/provider.js
+++ app/provider.js
@@ -1,9 +1,11 @@
 'use client'
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useUser} from '@clerk/nextjs'
+import { UserDetailsContext } from '@/context/UserContext'
 function Provider({children}) {
     const {user} = useUser()
+    const [userDetails, setUserDetails] = useState()
     useEffect(()=>{
         if(user){
             createNewUser()
@@ -18,7 +20,10 @@ function Provider({children}) {
     console.log("User created:", res.data);
     }
   return (
+    <UserDetailsContext.Provider value={{userDetails, setUserDetails}}>
+
     <div>{children}</div>
+    </UserDetailsContext.Provider>
   )
 }
 
